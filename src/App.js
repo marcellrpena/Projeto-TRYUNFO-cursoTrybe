c852535b8e0adb7eac17783aa29551278d346021
Marcell Rocha Pena <proartcarimbos@gmail.com.br>
2022-06-25 21:02:46 -0300

    concluído o requisito 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,63 @@ import React from 'react';
 import Form from './components/Form';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: false,
+    };
+  }
+
+  onSaveButtonClick = (event) => {
+    const { target } = event;
+    console.log(target);
+    /* this.setState({ isSaveButtonDisabled: target.value }); */
+  }
+
+  onInputChange = (event) => {
+    const { target } = event;
+    this.setState({
+      [target.name]: target.type === 'checkbox' ? target.checked : target.value });
+  }
+
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled } = this.state;
     return (
-      <div>
-        <h1>Adicionar Nova Carta</h1>
-        <Form />
+      <div className="card-Create">
+        <h1>Trunfo</h1>
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          hasTrunfo={ hasTrunfo }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+          onInputChange={ this.onInputChange }
+          onSaveButtonClick={ this.onSaveButtonClick }
+        />
       </div>
     );
   }
